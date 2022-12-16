de1bc3fc43473104fa4158d5f7e47384520709af
Bill Wohler <Bill.Wohler@nasa.gov>
2022-12-16 11:26:28 -0800

    ZIGGY-180: Fix mystery in FileAppenderTest by making log a local variable

--- src/test/java/gov/nasa/ziggy/services/logging/FileAppenderTest.java
+++ src/test/java/gov/nasa/ziggy/services/logging/FileAppenderTest.java
@@ -3,51 +3,35 @@ package gov.nasa.ziggy.services.logging;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.FileAppender;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import gov.nasa.ziggy.ZiggyDirectoryRule;
 import gov.nasa.ziggy.ZiggyPropertyRule;
 
 public class FileAppenderTest {
 
     private static final String LOG4J_CONFIG_FILE = "test/data/logging/log4j2.xml";
-    private static Logger log;
-    private File appenderFile;
-    private Path testClassDir;
+
+    @Rule
+    public ZiggyDirectoryRule directoryRule = new ZiggyDirectoryRule();
 
     @Rule
     public ZiggyPropertyRule log4j2ConfigurationFilePropertyRule = new ZiggyPropertyRule(
         "log4j2.configurationFile", LOG4J_CONFIG_FILE);
-
-    // Important note:
-    // For some reason, when this class is refactored to use the ZiggyDirectoryRule, the
-    // test fails. However, when the content of the ZiggyDirectoryRule is implemented in
-    // the class and its methods as discrete statements, the test passes! For now, we've
-    // decided to live with this mystery.
-    @Before
-    public void setup() throws IOException {
-        testClassDir = Paths.get("build", "test", "services.logging.FileAppenderTest");
-        Files.createDirectories(testClassDir);
-        log = LoggerFactory.getLogger(FileAppenderTest.class);
-    }
 
     @Test
     public void testFileAppender() throws IOException {
@@ -57,32 +41,26 @@ public class FileAppenderTest {
         LoggerConfig lc = c.getLoggerConfig("");
         lc.setLevel(Level.ALL);
 
-        Path testDir = testClassDir.resolve("testFileAppender");
-        Files.createDirectories(testDir);
-        appenderFile = testDir.resolve("file-appender.log").toFile();
-        if (appenderFile.exists()) {
-            appenderFile.delete();
-        }
+        Path appenderFile = directoryRule.directory().resolve("file-appender.log");
 
         FileAppender taskLog = FileAppender.newBuilder()
-            .withFileName(appenderFile.getAbsolutePath())
+            .withFileName(appenderFile.toString())
             .setName("file-appender.log")
             .build();
         taskLog.start();
         lc.addAppender(taskLog, Level.ALL, null);
 
+        Logger log = LoggerFactory.getLogger(FileAppenderTest.class);
         log.info("Here is a test message with INFO level");
         log.warn("Here is a test message with WARN level");
 
         lc.removeAppender("file-appender.log");
 
-        assertTrue(appenderFile.exists());
+        assertTrue(Files.exists(appenderFile));
 
-        List<String> logLines = FileUtils.readLines(appenderFile, (Charset) null);
+        List<String> logLines = Files.readAllLines(appenderFile);
         assertEquals(2, logLines.size());
         assertTrue(logLines.get(0).contains("Here is a test message with INFO level"));
         assertTrue(logLines.get(1).contains("Here is a test message with WARN level"));
-
     }
-
 }
