145dbdeb2beb7a45c1163b559dec2183fc48cbce
Bill Wohler <Bill.Wohler@nasa.gov>
2022-12-21 11:48:20 -0800

    ZIGGY-188: Add NASA notice to ClusterController

--- src/main/java/gov/nasa/ziggy/ui/ClusterController.java
+++ src/main/java/gov/nasa/ziggy/ui/ClusterController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 United States Government as represented by the Administrator of the National
+ * Copyright © 2022 United States Government as represented by the Administrator of the National
  * Aeronautics and Space Administration. All Rights Reserved.
  *
  * NASA acknowledges the SETI Institute's primary role in authoring and producing Ziggy, a Pipeline
