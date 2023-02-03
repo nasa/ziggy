47563f5e1077c92d8c36f9f0274696f90d7ffd7a
Bill Wohler <Bill.Wohler@nasa.gov>
2023-02-02 16:42:36 -0800

    ZIGGY-220: Change copyright years from 2022 to 2022-2023

--- src/main/java/gov/nasa/ziggy/ui/ClusterController.java
+++ src/main/java/gov/nasa/ziggy/ui/ClusterController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 United States Government as represented by the Administrator of the National
+ * Copyright (C) 2022-2023 United States Government as represented by the Administrator of the National
  * Aeronautics and Space Administration. All Rights Reserved.
  *
  * NASA acknowledges the SETI Institute's primary role in authoring and producing Ziggy, a Pipeline
